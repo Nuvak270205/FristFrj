63ab678ac96b1e24605170ac2dd0c1135ba35b26
Nguyễn Thái Dương <23701581.duong@student.iuh.edu.vn>
2024-08-15 21:31:11 +0700

    messager
    Signed-off-by: Nguyễn Thái Dương <23701581.duong@student.iuh.edu.vn>

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Dinh Hung!");
+        System.out.println("Welcone to the DHHTTT19A");
+    }
+}
